c6b91556043d97a31a6bc68149f1a787a1296f07
Shreyansh Katiyar <shreyanshkatiyar611@gmail.com>
2024-09-14 01:04:26 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { PiCopyright } from "react-icons/pi";
+import '../Styles/Footer.css'
+
 
 export const Footer = () => {
   return (
     <div className='Footer'>
-      <a href="#"></a>
-      <a href="#"></a>
-      <a href="#"></a>
-      <a href="#"></a>
-      <a href="#"></a>
-      <a href="#"></a>
+      <a href="#">About</a>
+      <a href="#">.Help</a>
+      <a href="#">.Privacy</a>
+      <a href="#">.Terms</a>
+      <a href="#">.Locations</a>
+      <a href="#">.Language</a>
       <br />
       <br />
       <h5><PiCopyright />Copyright 2024 Instagram</h5>
